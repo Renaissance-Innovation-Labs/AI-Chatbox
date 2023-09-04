9f115cb9d58ea5f0b855952b3fd49a6b3e936ec5
Jowwy02 <ukpejosephineabel@gmail.com>
2023-09-04 04:58:52 -0700

    Complete UI and add functionalities.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
-import OpenAIApi from "openai";
+import OpenAI from "openai";
 
-const openai = new OpenAIApi({
+const openai = new OpenAI({
   apiKey: process.env.REACT_APP_OPENAI_KEY,
   dangerouslyAllowBrowser: true,
 });
@@ -11,15 +11,86 @@ function App() {
   const [messages, setMessages] = useState([]);
   const [isThinking, setIsThinking] = useState(false);
 
+  console.log(messages);
+  const callOpenaiAPI = async () => {
+    setMessages([...messages, { role: "system", content: input }]);
+    setInput("");
+    try {
+      const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo-0613",
+        messages: [{ role: "system", content: input }],
+        max_tokens: 500,
+        temperature: 0,
+      });
+
+      setMessages((prev) => [...prev, completion.choices[0].message]);
+      setIsThinking(false);
+    } catch (error) {
+      setIsThinking(false);
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
+    }
+  };
+
   const onSubmitAction = () => {
     if (!input) return;
     setIsThinking(true);
-    setMessages([...messages, { role: "user", content: "input" }]);
+    setMessages([...messages, { role: "user", content: input }]);
+    callOpenaiAPI();
   };
+
+  // useEffect(() => {
+  //   input && callOpenaiAPI();
+  // }, [messages]);
+  const preloadSVG = (
+    <>
+      <svg
+        className="inline w-8 h-8 mr-2 text-white animate-spin fill-blue-600"
+        aria-hidden="true"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span className="text-white opacity-70">Loading...</span>
+    </>
+  );
 
   return (
     <>
-      <div className="relative max-w-3xl"></div>
+      <div className="relative max-w-3xl mx-auto min-h-[cal(100vh_-_150px)] pb-[170px]">
+        {messages.map((messages, i) => (
+          <>
+            <div
+              key={i}
+              className={`p-3 max-w-3xl overflow-y-auto whitespace-normal text-white my-2 ml-auto ${
+                messages.role !== "assistant"
+                  ? "bg-blue-600 rounded-lg"
+                  : "bg-gray-900 rounded-lg"
+              }`}
+            >
+              {messages.content}
+            </div>
+          </>
+        ))}
+        {isThinking && (
+          <div className="p-3 max-w-full text-white my-2 ml-auto">
+            {preloadSVG}
+          </div>
+        )}
+      </div>
       <div className="fixed bottom-0 bg-gray-900 flex items-center h-[150px] p-2 w-full md:p-8">
         <div className="mx-auto flex justify-bottom max-w-3xl">
           <textarea
@@ -28,10 +99,10 @@ function App() {
             onChange={(e) => setInput(e.target.value)}
             className="flex-grow p-2 rounded-lg mr-2 w-[70vw] md:text-xl"
             onKeyDown={(e) => {
-              e.preventDefault();
-              // if (e.key === "Enter" && e.shiftKey === false) {
-              //   input && onSubmitAction();
-              // }
+              if (e.key === "Enter" && e.shiftKey === false) {
+                e.preventDefault();
+                input && onSubmitAction();
+              }
             }}
           ></textarea>
           <button
@@ -42,6 +113,21 @@ function App() {
           </button>
         </div>
       </div>
+
+      {!messages.length && (
+        <div
+          className="absolute top-[200px] left-1/3
+          transform-translate-x-1/2 max-w-[500px]"
+        >
+          <h1 className="font-black text-white text-4xl">ChatBot</h1>
+          <p className="text-white mt-2">
+            Hello there! I'm Chatbot, a language model trained to assist you
+            with various topics and questions. What can i help you with today?
+            Feel free to type your query or topic of interest, and i'll do my
+            best to provide you with helpful and informative responses.
+          </p>
+        </div>
+      )}
     </>
   );
 }
